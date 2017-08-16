f0a030ce51e0fc21b9abd0be6c0b88a9b5f11e3d
Alex <alexandersmith223@gmail.com>
2017-08-16 12:29:37 -0400

    Add S3 host_name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_region => ENV['AWS_REGION'],
     :s3_protocol => 'https',
+    :s3_host_name => ENV['S3_HOST_NAME'],
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
